07dcc7f88e0732b26493e3eeeae0c78fb77a93b1
fornost <fornost461@users.noreply.github.com>
2016-07-15 00:26:20 +0200

    stuff

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 cd 'files'      # move into directory to set it as root of the archive
 destination='../bin'
 mkdir -p "${destination}"
-zip -9 -r "${destination}/tetris-clone-0.1.0.love" .
+zip -9 -r "${destination}/tetris-clone.love" .
